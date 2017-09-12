c8b422350e9a421d1b062e96fa0f144bc82aee47
r0cco <Rocco.Riccardi.42@gmail.com>
2017-09-12 12:59:30 -0400

    added a paddle w mouse controls

--- js/breakout.js
+++ js/breakout.js
@@ -7,12 +7,15 @@ var game = new Phaser.Game(
 	
 var ball;
 
+var paddle;
+
 function preload() {
 	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 	game.scale.pageAlignHorizontally = true;
 	game.scale.pageAlignVertically = true;
 	game.stage.backgroundColor = '#eee';
 	game.load.image('ball', './img/ball.png');
+	game.load.image('paddle', 'img/paddle.png');
 }
 	
 function create() {
@@ -21,10 +24,16 @@ function create() {
 	game.physics.enable(ball, Phaser.Physics.ARCADE);
 	ball.body.collideWorldBounds = true;
 	ball.body.bounce.set(1);
-	ball.body.velocity.set(150, 150);
+	ball.body.velocity.set(150, -150);
+	paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, 'paddle');
+	paddle.anchor.set(0.5,1);
+	game.physics.enable(paddle, Phaser.Physics.ARCADE);
+	paddle.body.immovable = true;
 }
 
 function update() {
 	ball.x += 1;
-    ball.y += 1;
+	ball.y += 1;
+	game.physics.arcade.collide(ball, paddle);
+	paddle.x = game.input.x || game.world.width*0.5;
 }
