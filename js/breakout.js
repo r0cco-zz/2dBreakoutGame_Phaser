f18aa261787f58011e23ebf8d696674859d76c09
r0cco <Rocco.Riccardi.42@gmail.com>
2017-09-22 22:18:40 -0400

    added brick pic, building brick field. No collisions yet

--- js/breakout.js
+++ js/breakout.js
@@ -9,13 +9,18 @@ var ball;
 
 var paddle;
 
+var bricks;
+var newBrick;
+var brickInfo;
+
 function preload() {
 	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 	game.scale.pageAlignHorizontally = true;
 	game.scale.pageAlignVertically = true;
 	game.stage.backgroundColor = '#eee';
 	game.load.image('ball', './img/ball.png');
-	game.load.image('paddle', 'img/paddle.png');
+	game.load.image('paddle', './img/paddle.png');
+	game.load.image('brick', './img/brick.png');
 }
 	
 function create() {
@@ -35,6 +40,7 @@ function create() {
 	paddle.anchor.set(0.5,1);
 	game.physics.enable(paddle, Phaser.Physics.ARCADE);
 	paddle.body.immovable = true;
+	initBricks();
 }
 
 function update() {
@@ -42,4 +48,33 @@ function update() {
 	ball.y += 1;
 	game.physics.arcade.collide(ball, paddle);
 	paddle.x = game.input.x || game.world.width*0.5;
+}
+
+function initBricks() {
+    brickInfo = {
+        width: 50,
+        height: 20,
+        count: {
+            row: 7,
+            col: 3
+        },
+        offset: {
+            top: 50,
+            left: 60
+        },
+        padding: 10
+	};
+	bricks = game.add.group();
+
+	for (c = 0; c < brickInfo.count.col; c++) {
+		for(r = 0; r < brickInfo.count.row; r++) {
+			var brickX = (r * (brickInfo.width + brickInfo.padding)) + brickInfo.offset.left;
+			var brickY = (c * (brickInfo.height + brickInfo.padding)) + brickInfo.offset.top;
+			newBrick = game.add.sprite(brickX, brickY, 'brick');
+			game.physics.enable(newBrick, Phaser.Physics.ARCADE);
+			newBrick.body.immovable = true;
+			newBrick.anchor.set(0.5);
+			bricks.add(newBrick);
+		}
+	}
 }
