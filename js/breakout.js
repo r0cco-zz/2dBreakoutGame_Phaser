9df46cecfec07e189f10f598320ceee10f4feb8e
r0cco <Rocco.Riccardi.42@gmail.com>
2017-09-22 23:53:07 -0400

    added start button, ball now serves to paddle, variance in how ball bounces based on how it strikes the paddle

--- js/breakout.js
+++ js/breakout.js
@@ -20,6 +20,9 @@ var lives = 3;
 var livesText;
 var lifeLostText;
 
+var playing = false;
+var startButton;
+
 function preload() {
 	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 	game.scale.pageAlignHorizontally = true;
@@ -29,18 +32,20 @@ function preload() {
 	game.load.image('paddle', './img/paddle.png');
 	game.load.image('brick', './img/brick.png');
 	game.load.spritesheet('ball', './img/wobble.png', 20, 20);
+	game.load.spritesheet('button', './img/button.png', 120, 40);
 }
 	
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	ball = game.add.sprite(50, 50, 'ball');
+	game.physics.arcade.checkCollision.down = false;
+
+	ball = game.add.sprite(game.world.width * 0.5, game.world.height - 50, 'ball');
 	ball.animations.add('wobble', [0,1,0,2,0,1,0,2,0], 24);
+	ball.anchor.set(0.5);
 	game.physics.enable(ball, Phaser.Physics.ARCADE);
 	ball.body.collideWorldBounds = true;
 	ball.body.bounce.set(1);
-	ball.body.velocity.set(150, -150);
-	game.physics.arcade.checkCollision.down = false;
 	ball.checkWorldBounds = true;
 	ball.events.onOutOfBounds.add(ballLeaveScreen, this);
 
@@ -58,14 +63,17 @@ function create() {
 	lifeLostText = game.add.text(game.world.width*0.5, game.world.height*0.5, 'Life lost, click to continue', textStyle);
 	lifeLostText.anchor.set(0.5);
 	lifeLostText.visible = false;
+
+	startButton = game.add.button(game.world.width*0.5, game.world.height*0.5, 'button', startGame, this, 1, 0, 2);
+	startButton.anchor.set(0.5);
 }
 
 function update() {
-	ball.x += 1;
-	ball.y += 1;
 	game.physics.arcade.collide(ball, paddle, ballHitPaddle);
 	game.physics.arcade.collide(ball, bricks, ballHitBrick);
-	paddle.x = game.input.x || game.world.width * 0.5;
+	if (playing) {
+        paddle.x = game.input.x || game.world.width * 0.5;
+    }
 }
 
 function initBricks() {
@@ -98,7 +106,8 @@ function initBricks() {
 }
 
 function ballHitPaddle() {
-	//ball.animations.play('wobble');
+	ball.animations.play('wobble');
+	ball.body.velocity.x = -1 * 5 * (paddle.x - ball.x);
 }
 
 function ballHitBrick(ball, brick) {
@@ -113,17 +122,11 @@ function ballHitBrick(ball, brick) {
 
 	score += 10;
 	scoreText.setText('Points: '+score);
-	
-	var count_alive = 0;
-    for (i = 0; i < bricks.children.length; i++) {
-    	if (bricks.children[i].alive == true) {
-        	count_alive++;
-    	}
-    }
-    if (count_alive == 0) {
-    	alert('A winner is you!');
-    	location.reload();
-    }
+
+	if (score === brickInfo.count.row * brickInfo.count.col * 10) {
+		alert('A winner is you!!1!');
+		location.reload();
+	}
 }
 
 function ballLeaveScreen() {
@@ -131,15 +134,21 @@ function ballLeaveScreen() {
     if(lives) {
         livesText.setText('Lives: '+lives);
         lifeLostText.visible = true;
-        ball.reset(game.world.width*0.5, game.world.height*0.5);
-        paddle.reset(game.world.width*0.5, game.world.height-5);
+        ball.reset(game.world.width*0.5, game.world.height - 50);
+        paddle.reset(game.world.width*0.5, game.world.height - 5);
         game.input.onDown.addOnce(function(){
             lifeLostText.visible = false;
-            ball.body.velocity.set(150, -150);
+            ball.body.velocity.set(150, 150);
         }, this);
     }
     else {
         alert('You lost, game over!');
         location.reload();
     }
+}
+
+function startGame() {
+    startButton.destroy();
+    ball.body.velocity.set(150, 150);
+    playing = true;
 }
