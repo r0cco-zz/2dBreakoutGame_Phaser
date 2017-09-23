d765ac11f3aea5be9c69360a6a2fe9183439281e
r0cco <Rocco.Riccardi.42@gmail.com>
2017-09-22 23:20:44 -0400

    added wobble animation

--- js/breakout.js
+++ js/breakout.js
@@ -28,11 +28,14 @@ function preload() {
 	game.load.image('ball', './img/ball.png');
 	game.load.image('paddle', './img/paddle.png');
 	game.load.image('brick', './img/brick.png');
+	game.load.spritesheet('ball', './img/wobble.png', 20, 20);
 }
 	
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
+
 	ball = game.add.sprite(50, 50, 'ball');
+	ball.animations.add('wobble', [0,1,0,2,0,1,0,2,0], 24);
 	game.physics.enable(ball, Phaser.Physics.ARCADE);
 	ball.body.collideWorldBounds = true;
 	ball.body.bounce.set(1);
@@ -40,11 +43,14 @@ function create() {
 	game.physics.arcade.checkCollision.down = false;
 	ball.checkWorldBounds = true;
 	ball.events.onOutOfBounds.add(ballLeaveScreen, this);
+
 	paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, 'paddle');
 	paddle.anchor.set(0.5,1);
 	game.physics.enable(paddle, Phaser.Physics.ARCADE);
 	paddle.body.immovable = true;
+
 	initBricks();
+
 	textStyle = { font: '18px Arial', fill: '#0095DD' };
 	scoreText = game.add.text(5, 5, 'Points: 0', textStyle);
 	livesText = game.add.text(game.world.width-5, 5, 'Lives: '+lives, textStyle);
@@ -57,9 +63,9 @@ function create() {
 function update() {
 	ball.x += 1;
 	ball.y += 1;
-	game.physics.arcade.collide(ball, paddle);
+	game.physics.arcade.collide(ball, paddle, ballHitPaddle);
 	game.physics.arcade.collide(ball, bricks, ballHitBrick);
-	paddle.x = game.input.x || game.world.width*0.5;
+	paddle.x = game.input.x || game.world.width * 0.5;
 }
 
 function initBricks() {
@@ -91,8 +97,20 @@ function initBricks() {
 	}
 }
 
+function ballHitPaddle() {
+	//ball.animations.play('wobble');
+}
+
 function ballHitBrick(ball, brick) {
-	brick.kill();
+	ball.animations.play('wobble');
+
+	var killTween = game.add.tween(brick.scale);
+	killTween.to({x:0,y:0}, 200, Phaser.Easing.Linear.None);
+	killTween.onComplete.addOnce(function(){
+		brick.kill();
+	}, this);
+	killTween.start();
+
 	score += 10;
 	scoreText.setText('Points: '+score);
 	
@@ -103,7 +121,7 @@ function ballHitBrick(ball, brick) {
     	}
     }
     if (count_alive == 0) {
-    	alert('You won the game, congratulations!');
+    	alert('A winner is you!');
     	location.reload();
     }
 }
